7d912aa547a3ea05dbe4a847ead0b3ba5ac3e8d6
Sabeer-Alam961 <alamsabeer05@gmail.com>
2026-06-13 11:39:42 +0500

    Lear for loop and while loop

---
+++ JavaScript/03.Loops/for-loop.js
@@ -0,0 +1,26 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${i} Welcome`);
+// }
+
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(`${i} Welcome`);
+// }
+
+// Checking even number using for loop
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     };
+// }
+
+// for(let i = 2; i <= 100; i+=2){
+//     console.log(i);
+// }
+
+// Checking odd number
+for(i = 1; i <= 10; i+=2){
+    console.log(i);
+}
+
---
+++ JavaScript/03.Loops/while-loop.js
@@ -0,0 +1,23 @@
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+// while (i <= 20) {
+//     console.log(i);
+//     i+=2; // i = i + 2
+// }
+
+// let i = 1;
+// while (i <= 20) {
+//     console.log(i);
+//     i+=2; // i = i + 2
+// }
+
+let i = 30;
+while(i >= 1){
+    console.log(i);
+    i--;
+}
