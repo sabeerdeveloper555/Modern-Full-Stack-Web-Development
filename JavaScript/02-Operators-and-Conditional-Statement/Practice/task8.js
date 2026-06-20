ea4d9f053066f6282e10d802c919269924d39a73
Sabeer-Alam961 <alamsabeer05@gmail.com>
2026-06-20 12:05:10 +0500

    Practice Increment and Decrement Operator

---
+++ JavaScript/02-Operators-and-Conditional-Statement/Practice/task8.js
@@ -0,0 +1,44 @@
+// let a = 5;
+// let b = 3;
+
+// let result = a + b-- * ++a + b / --b - a++;
+
+// console.log("result: ", result);
+// console.log("a: ", a);
+// console.log("b: ", b);
+
+// let x = 4;
+// let y = 2;
+
+// let result = x++ + --y * ++x - y++ + x / y;
+
+// console.log("result: ", result);
+// console.log("x: ", x);
+// console.log("y: ", y);
+
+// let m = 8;
+// let n = 5;
+
+// let result = ++m - n-- + m * --n + n++ - m--;
+
+// console.log("result: ", result);
+// console.log("m: ", m);
+// console.log("n: ", n);
+
+// let p = 7;
+// let q = 3;
+
+// let result = p++ * ++q - --p + q-- + p / q;
+
+// console.log("result: ", result);
+// console.log("p: ", p);
+// console.log("q: ", q);
+
+let x6 = 10;
+let y6 = 4;
+
+let result6 = ++x6 + y6-- * x6++ - --y6 + x6 / ++y6 - x6--;
+
+console.log("result: ", result6);
+console.log("x6: ", x6);
+console.log("y6: ", y6);
