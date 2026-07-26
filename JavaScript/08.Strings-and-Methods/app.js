eb8ab19b5e6a8f340e1ae59f1c9a9aed3de1c5ca
Sabeer-Alam961 <alamsabeer05@gmail.com>
2026-07-26 14:59:23 +0500

    Strings and Methods

---
+++ JavaScript/08.Strings-and-Methods/app.js
@@ -0,0 +1,65 @@
+//String: string is a sequence of character enclosed in quotes
+
+// let name = "Sabeer";
+// let city = "Karachi"
+
+// console.log(`Name: ${name} \nCity: ${city}`);
+
+//1. length Property: returns a total number of characters
+
+let userName = "Sabeer Alam";
+let password = "abc12345";
+console.log(`The length of username is ${userName.length}`);
+console.log(`The length of password is ${password.length}`);
+
+//2. toUpperCase(): converts all letters to uppercase
+let capital = "islamabad";
+console.log(capital.toUpperCase());
+
+//3. toLowerCase(): converts text into lowercase
+let email = "SABEER@EXAMPLE.COM";
+console.log(email.toLowerCase());
+
+//4. trim(): remove extra spaces.
+let name = " Muhammad Sabeer Alam ";
+console.log(name.trim());
+
+//5. includes(): check whether text exists
+let sentence = "I love JavaScript";
+console.log(sentence.includes("Java"));
+
+//6. startsWith(): check beginning
+// let file = "image-png";
+// console.log(file.startsWith("image"));
+// let word = "JavaScript";
+// console.log(word.startsWith("Java"));
+
+//7. endsWith(): checks ending
+let userEmail = "abc@gmail.com";
+console.log(userEmail.endsWith(".com"));
+
+let file = "resume.pdf";
+console.log(file.endsWith(".pdf"));
+
+//8. replace(): replaces the first occurance
+let text1 = "I like tea";
+console.log(text1.replace("tea", "coffee"));
+let text2 = "I like C#";
+console.log(text2.replace("C#", "JavaScript"));
+
+//9. split(): converts string into array
+let fruits = "Apple, Banana, Mango, Orange, Grapes";
+console.log(fruits.split(","));
+
+//10. charAt(): returns character
+let programmingLanguage = "JavaScript";
+console.log(programmingLanguage.charAt(3));
+
+//11. indexOf(): returns position
+let word = "JavaScript";
+console.log(word.indexOf("S"));
+
+//12. repeat(): repeats string
+let str = "Python ";
+console.log(str.repeat(5));
+
